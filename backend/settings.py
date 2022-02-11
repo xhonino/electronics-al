0ae0ac4f6882d059f89c70724edc757d0c4564d6
Xhonino Deda <dedaxhonino@yahoo.com>
2022-02-11 18:17:37 +0100

    ...

--- backend/settings.py
+++ backend/settings.py
@@ -194,13 +194,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Dont forget to reset database connection and hide password
-AWS_QUERYSTRING_AUTH = False
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_QUERYSTRING_AUTH = False
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = 'electronical-bucket-demo'
+# AWS_STORAGE_BUCKET_NAME = 'electronical-bucket-demo'
 
 
 if os.getcwd() == '/app':
