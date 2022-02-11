210ba00929aa435d1a7daf6716e7180fee64012d
Xhonino Deda <dedaxhonino@yahoo.com>
2022-02-11 20:26:18 +0100

    ...

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'electronics-al.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
